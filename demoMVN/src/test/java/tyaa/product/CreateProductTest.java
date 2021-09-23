6d43e5bee6a81c4ccd847c00da07f84a4ab95374
pradeep-git135 <pradeepcr135@gmail.com>
2021-09-23 15:11:03 +0530

    commit 2

--- demoMVN/src/test/java/tyaa/product/CreateProductTest.java
+++ demoMVN/src/test/java/tyaa/product/CreateProductTest.java
@@ -6,6 +6,11 @@ public class CreateProductTest {
 	@Test
 	public void createProduct() {
 		System.out.println("executing");
+		String browser = System.getProperty("browser");
+		System.out.println("================================================browser is : "+browser);
+		String url = System.getProperty("url");
+		System.out.println("================================================url is : "+url);
+
 	}
 	@Test
 	public void createProductwithInfo() {
